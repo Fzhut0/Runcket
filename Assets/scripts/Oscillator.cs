f244665e466c81d7157a90c8d089bc1dcf44b5f8
Fzhut0 <80861162+Fzhut0@users.noreply.github.com>
2022-01-19 19:43:36 +0100

    Project settings

--- Assets/scripts/Oscillator.cs
+++ Assets/scripts/Oscillator.cs
@@ -9,14 +9,12 @@ public class Oscillator : MonoBehaviour
     [SerializeField] Vector3 movementVector;
     [SerializeField] [Range(0, 1)] float movementFactor;
     [SerializeField] float period = 2f;
-    // Start is called before the first frame update
     void Start()
     {
         startingPos = transform.position;
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (period <= Mathf.Epsilon) { return; }
